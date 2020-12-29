78c2ed5207ae67c9e6f99f6603347f9b978e5324
majun <95408719@qq.com>
2020-12-29 17:10:49 +0800

    提交

--- main_test.go
+++ main_test.go
@@ -7,44 +7,43 @@ import (
 )
 
 func Test(t *testing.T) {
-	ROOT,_ := os.Getwd()
-	path := ROOT+"/demo/mapper"
-	v,_ := ReaderConfigBuilder(path)
+	ROOT, _ := os.Getwd()
+	path := ROOT + "/demo/mapper"
+	v, _ := ReaderConfigBuilder(path)
 
 	//获取sql
-	m,err := v.GetMethodSql("demo.mapper.user.getuser")
-	if err!=nil{
+	m, err := v.GetMethodSql("demo.mapper.user.getuser")
+	if err != nil {
 		println(err.Error())
 		return
 	}
 	println(m.Sql)
 	//把sql转成可以执行的sql
 	params := map[string]interface{}{
-		"pwd":"222",
+		"pwd": "222", "name": "",
 	}
-	sql1,sqlParams,err := GetExecSqlInfo(m.Sql,params)
-	if err !=nil{
+	sql1, sqlParams, err := GetExecSqlInfo(m.Sql, params)
+	if err != nil {
 		println(err.Error())
 		return
 	}
 	println(sql1)
 	println(sqlParams)
 
-
 }
 
 //验证xml
-func Test_Validate(t *testing.T)  {
-	ROOT,_ := os.Getwd()
-	path := ROOT+"/demo/mapper/aaMapper.xml"
+func Test_Validate(t *testing.T) {
+	ROOT, _ := os.Getwd()
+	path := ROOT + "/demo/mapper/aaMapper.xml"
 	file, err := os.Open(path)
 	if nil != err {
 		panic("Open mapper config: " + path + " err:" + err.Error())
 	}
 	defer file.Close()
 	parser := xml.NewDecoder(file)
-	startTag := make([]string,1)
-	endTag   := make([]string,1)
+	startTag := make([]string, 1)
+	endTag := make([]string, 1)
 	for {
 		token, err := parser.Token()
 		if err != nil {
@@ -54,23 +53,22 @@ func Test_Validate(t *testing.T)  {
 		case xml.StartElement: //tag start
 			elmt1 := xml.StartElement(t)
 			name := elmt1.Name.Local
-			if  name !="sqlMapper" && name !="update" && name !="select" && name !="delete" && name !="insert" {
-				panic("sql mapper tag error,please check your xml  "+path)
-			}else{
-				startTag = append(startTag,name)
+			if name != "sqlMapper" && name != "update" && name != "select" && name != "delete" && name != "insert" {
+				panic("sql mapper tag error,please check your xml  " + path)
+			} else {
+				startTag = append(startTag, name)
 			}
 		case xml.EndElement: //tag end
 			elmt2 := xml.EndElement(t)
 			name := elmt2.Name.Local
-			if  name !="sqlMapper" && name !="update" && name !="select" && name !="delete" && name !="insert" {
-				panic("sql mapper tag error,please check your xml "+path)
-			}else{
-				endTag = append(endTag,name)
+			if name != "sqlMapper" && name != "update" && name != "select" && name != "delete" && name != "insert" {
+				panic("sql mapper tag error,please check your xml " + path)
+			} else {
+				endTag = append(endTag, name)
 			}
 		}
 	}
-	if len(startTag)!=len(endTag){
+	if len(startTag) != len(endTag) {
 		panic("xml error, there is tag no close, please check your xml config!")
 	}
 }
-
